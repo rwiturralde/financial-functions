f983184a16989f39065a3b0211715facabe7bd1c
iturrar <iturrar@amazon.com>
2018-03-01 16:48:29 -0500

    First pass at README description and usage examples. Also improved argument validation in function code.

---
+++ README.md
@@ -0,0 +1,41 @@
+#NumPy Wrapper
+Simple [AWS Lambda](https://aws.amazon.com/lambda) function that wraps the popular python library, [NumPy](http://www.numpy.org/).
+
+## Usage
+
+The AWS Lambda function event expects two arguments: 'method' and 'arguments'.
+* method - String - The NumPy method to invoke
+* arguments - Array - An ordered array of arguments to pass to the method mentioned above.
+
+## Examples
+
+Computing [future value](https://docs.scipy.org/doc/numpy/reference/generated/numpy.fv.html) 
+```json
+{
+  "method": "fv",
+  "arguments": [
+    0.004166666666666666,
+    120,
+    -100,
+    -100
+  ]
+}
+```
+
+Computing the [net present value](https://docs.scipy.org/doc/numpy/reference/generated/numpy.npv.html) of a cash flow series.
+```json
+{
+  "method": "npv",
+  "arguments": [
+    0.281,
+    [
+      -100,
+      39,
+      59,
+      55,
+      20
+    ]
+  ]
+}
+```
+##
--- code/handler.py
+++ code/handler.py
@@ -15,6 +15,7 @@ if not isinstance(numeric_level, int):
 logger = logging.getLogger(__name__)
 logger.setLevel(numeric_level)
 
+
 def lambda_handler(event, context):
     """
     Delegate method and arguments to NumPy
@@ -23,13 +24,24 @@ def lambda_handler(event, context):
     event.arguments -- Array of ordered arguments to pass to the above NumPy method
     """
 
+    # Check that we were passed the required arguments
     if 'method' in event and 'arguments' in event:
         numpy_method_name = event.get('method')
         numpy_argument_array = event.get('arguments')
-        logger.info("Handing call to the NumPy {} method with arguments: {}".format(numpy_method_name, numpy_argument_array))
-        result = getattr(numpy, numpy_method_name)(*numpy_argument_array)
-        logger.info("Result from NumPy is {}".format(result))
-        return({'result':result})
+
+        # Check that the arguments are valid
+        if hasattr(numpy, numpy_method_name) and callable(getattr(numpy, numpy_method_name)):
+            logger.info("Handing call to the NumPy {} method with arguments: {}".format(numpy_method_name, numpy_argument_array))
+            result = getattr(numpy, numpy_method_name)(*numpy_argument_array)
+            logger.info("Result from NumPy is {}".format(result))
+            return {'result': result}
+        else:
+            error_message = "Invalid NumPy method: {}".format(numpy_method_name)
+            logger.error(error_message)
+            raise AttributeError(error_message)
+
+
+
     else:
         error_message = "Missing required argument(s). Event must contain fields for \'method\' and \'arguments\'"
         logger.error(error_message)
