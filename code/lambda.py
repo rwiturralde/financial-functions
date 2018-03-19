6d428286b60848a42874558c25bcfbcc7ad2c9bb
iturrar <iturrar@amazon.com>
2018-03-19 15:08:32 -0400

    Add Present Value (PV) support.

--- code/lambda.py
+++ code/lambda.py
@@ -6,10 +6,20 @@ import numpy
 
 logger = log_helper.getLogger(__name__)
 
+
 def fv_handler(request, context):
     # TODO: validate arguments
 
     logger.info("FV request: {}".format(request))
     args = [request['rate'], request['nper'], request['pmt'], request.get('pv', 0), request.get('type', 0)]
     logger.info("Calling numpy.fv with args: {}".format(args))
-    return { 'result': numpy.fv(*args) }
+    return {'result': numpy.fv(*args)}
+
+
+def pv_handler(request, context):
+    # TODO: validate arguments
+
+    logger.info("PV request: {}".format(request))
+    args = [request['rate'], request['nper'], request['pmt'], request.get('fv', 0), request.get('type', 0)]
+    logger.info("Calling numpy.pv with args: {}".format(args))
+    return {'result': numpy.pv(*args)}
