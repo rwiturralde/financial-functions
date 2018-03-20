1be310a1b6b028537e2dd6ffcf9466b3f6320e51
iturrar <iturrar@amazon.com>
2018-03-20 10:10:23 -0400

    Adding support for the following financial functions: * IRR * MIRR * NPER * NPV * PMT * PPMT * Rate

--- code/lambda.py
+++ code/lambda.py
@@ -23,3 +23,67 @@ def pv_handler(request, context):
     args = [request['rate'], request['nper'], request['pmt'], request.get('fv', 0), request.get('type', 0)]
     logger.info("Calling numpy.pv with args: {}".format(args))
     return {'result': numpy.pv(*args)}
+
+
+def npv_handler(request, context):
+    # TODO: validate arguments
+
+    logger.info("NPV request: {}".format(request))
+    args = [request['rate'], request['values']]
+    logger.info("Calling numpy.npv with args: {}".format(args))
+    return {'result': numpy.npv(*args)}
+
+
+def pmt_handler(request, context):
+    # TODO: validate arguments
+
+    logger.info("PMT request: {}".format(request))
+    args = [request['rate'], request['nper'], request['pv'], request.get('fv', 0), request.get('type', 0)]
+    logger.info("Calling numpy.pmt with args: {}".format(args))
+    return {'result': numpy.pmt(*args)}
+
+
+def ppmt_handler(request, context):
+    # TODO: validate arguments
+
+    logger.info("PPMT request: {}".format(request))
+    args = [request['rate'], request['per'], request['nper'], request['pv'], request.get('fv', 0), request.get('type', 0)]
+    logger.info("Calling numpy.ppmt with args: {}".format(args))
+    return {'result': numpy.ppmt(*args)}
+
+
+def irr_handler(request, context):
+    # TODO: validate arguments
+
+    logger.info("IRR request: {}".format(request))
+    args = [request['values']]
+    logger.info("Calling numpy.irr with args: {}".format(args))
+    return {'result': numpy.irr(*args)}
+
+
+def mirr_handler(request, context):
+    # TODO: validate arguments
+
+    logger.info("MIRR request: {}".format(request))
+    args = [request['values'], request['finance_rate'], request['reinvest_rate']]
+    logger.info("Calling numpy.mirr with args: {}".format(args))
+    return {'result': numpy.mirr(*args)}
+
+
+def nper_handler(request, context):
+    # TODO: validate arguments
+
+    logger.info("NPER request: {}".format(request))
+    args = [request['rate'], request['pmt'], request['pv'], request.get('fv', 0), request.get('type', 0)]
+    logger.info("Calling numpy.nper with args: {}".format(args))
+    return {'result': numpy.nper(*args)}
+
+
+def rate_handler(request, context):
+    # TODO: validate arguments
+
+    logger.info("Rate request: {}".format(request))
+    args = [request['nper'], request['pmt'], request['pv'], request.get('fv', 0), request.get('type', 0), request.get('guess', 0.10)]
+    logger.info("Calling numpy.rate with args: {}".format(args))
+    return {'result': numpy.rate(*args)}
+
