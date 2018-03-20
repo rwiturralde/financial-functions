c0c96931c42ed411694721d7cf0b83b9fa2cde87
iturrar <iturrar@amazon.com>
2018-03-20 19:49:09 -0400

    - Using jsonschema for argument validation to the Lambda handlers. - Fixing bug in SAM templates referring to wrong code location after moving files.

--- code/lambda.py
+++ code/lambda.py
@@ -3,87 +3,134 @@ import sys
 import log_helper
 sys.path.append('lib')
 import numpy
+from jsonschema import validate
+import validation_json_schemas as schemas
 
 logger = log_helper.getLogger(__name__)
 
 
 def fv_handler(request, context):
-    # TODO: validate arguments
+    schema = schemas.fv_schema
 
-    logger.info("FV request: {}".format(request))
-    args = [request['rate'], request['nper'], request['pmt'], request.get('pv', 0), request.get('type', 0)]
-    logger.info("Calling numpy.fv with args: {}".format(args))
-    return {'result': numpy.fv(*args)}
+    try:
+        validate(request, schema)
+        logger.info("FV request: {}".format(request))
+        args = [request['rate'], request['nper'], request['pmt'], request.get('pv', 0), request.get('type', 0)]
+        logger.info("Calling numpy.fv with args: {}".format(args))
+        return {'result': numpy.fv(*args)}
+    except Exception as ex:
+        logger.error("Encountered an error while handling FV request with args: {}. Exception: {}".format(request, ex))
+        raise ex
 
 
 def pv_handler(request, context):
-    # TODO: validate arguments
+    schema = schemas.pv_schema
 
-    logger.info("PV request: {}".format(request))
-    args = [request['rate'], request['nper'], request['pmt'], request.get('fv', 0), request.get('type', 0)]
-    logger.info("Calling numpy.pv with args: {}".format(args))
-    return {'result': numpy.pv(*args)}
+    try:
+        validate(request, schema)
+        logger.info("PV request: {}".format(request))
+        args = [request['rate'], request['nper'], request['pmt'], request.get('fv', 0), request.get('type', 0)]
+        logger.info("Calling numpy.pv with args: {}".format(args))
+        return {'result': numpy.pv(*args)}
+    except Exception as ex:
+        logger.error("Encountered an error while handling PV request with args: {}. Exception: {}".format(request, ex))
+        raise ex
 
 
 def npv_handler(request, context):
-    # TODO: validate arguments
+    schema = schemas.npv_schema
 
-    logger.info("NPV request: {}".format(request))
-    args = [request['rate'], request['values']]
-    logger.info("Calling numpy.npv with args: {}".format(args))
-    return {'result': numpy.npv(*args)}
+    try:
+        validate(request, schema)
+        logger.info("NPV request: {}".format(request))
+        args = [request['rate'], request['values']]
+        logger.info("Calling numpy.npv with args: {}".format(args))
+        return {'result': numpy.npv(*args)}
+    except Exception as ex:
+        logger.error("Encountered an error while handling NPV request with args: {}. Exception: {}".format(request, ex))
+        raise ex
 
 
 def pmt_handler(request, context):
-    # TODO: validate arguments
+    schema = schemas.pmt_schema
 
-    logger.info("PMT request: {}".format(request))
-    args = [request['rate'], request['nper'], request['pv'], request.get('fv', 0), request.get('type', 0)]
-    logger.info("Calling numpy.pmt with args: {}".format(args))
-    return {'result': numpy.pmt(*args)}
+    try:
+        validate(request, schema)
+        logger.info("PMT request: {}".format(request))
+        args = [request['rate'], request['nper'], request['pv'], request.get('fv', 0), request.get('type', 0)]
+        logger.info("Calling numpy.pmt with args: {}".format(args))
+        return {'result': numpy.pmt(*args)}
+    except Exception as ex:
+        logger.error("Encountered an error while handling PMT request with args: {}. Exception: {}".format(request, ex))
+        raise ex
 
 
 def ppmt_handler(request, context):
-    # TODO: validate arguments
+    schema = schemas.ppmt_schema
 
-    logger.info("PPMT request: {}".format(request))
-    args = [request['rate'], request['per'], request['nper'], request['pv'], request.get('fv', 0), request.get('type', 0)]
-    logger.info("Calling numpy.ppmt with args: {}".format(args))
-    return {'result': numpy.ppmt(*args)}
+    try:
+        validate(request, schema)
+        logger.info("PPMT request: {}".format(request))
+        args = [request['rate'], request['per'], request['nper'], request['pv'], request.get('fv', 0), request.get('type', 0)]
+        logger.info("Calling numpy.ppmt with args: {}".format(args))
+        return {'result': numpy.ppmt(*args)}
+    except Exception as ex:
+        logger.error("Encountered an error while handling PPMT request with args: {}. Exception: {}".format(request, ex))
+        raise ex
 
 
 def irr_handler(request, context):
-    # TODO: validate arguments
+    schema = schemas.irr_schema
 
-    logger.info("IRR request: {}".format(request))
-    args = [request['values']]
-    logger.info("Calling numpy.irr with args: {}".format(args))
-    return {'result': numpy.irr(*args)}
+    try:
+        validate(request, schema)
+        logger.info("IRR request: {}".format(request))
+        args = [request['values']]
+        logger.info("Calling numpy.irr with args: {}".format(args))
+        return {'result': numpy.irr(*args)}
+    except Exception as ex:
+        logger.error("Encountered an error while handling IRR request with args: {}. Exception: {}".format(request, ex))
+        raise ex
 
 
 def mirr_handler(request, context):
-    # TODO: validate arguments
+    schema = schemas.mirr_schema
 
-    logger.info("MIRR request: {}".format(request))
-    args = [request['values'], request['finance_rate'], request['reinvest_rate']]
-    logger.info("Calling numpy.mirr with args: {}".format(args))
-    return {'result': numpy.mirr(*args)}
+    try:
+        validate(request, schema)
+        logger.info("MIRR request: {}".format(request))
+        args = [request['values'], request['finance_rate'], request['reinvest_rate']]
+        logger.info("Calling numpy.mirr with args: {}".format(args))
+        return {'result': numpy.mirr(*args)}
+    except Exception as ex:
+        logger.error("Encountered an error while handling MIRR request with args: {}. Exception: {}".format(request, ex))
+        raise ex
 
 
 def nper_handler(request, context):
-    # TODO: validate arguments
+    schema = schemas.nper_schema
 
-    logger.info("NPER request: {}".format(request))
-    args = [request['rate'], request['pmt'], request['pv'], request.get('fv', 0), request.get('type', 0)]
-    logger.info("Calling numpy.nper with args: {}".format(args))
-    return {'result': numpy.nper(*args)}
+    try:
+        validate(request, schema)
+        logger.info("NPER request: {}".format(request))
+        args = [request['rate'], request['pmt'], request['pv'], request.get('fv', 0), request.get('type', 0)]
+        logger.info("Calling numpy.nper with args: {}".format(args))
+        return {'result': numpy.nper(*args)}
+    except Exception as ex:
+        logger.error("Encountered an error while handling NPER request with args: {}. Exception: {}".format(request, ex))
+        raise ex
 
 
 def rate_handler(request, context):
-    # TODO: validate arguments
+    schema = schemas.rate_schema
 
-    logger.info("Rate request: {}".format(request))
-    args = [request['nper'], request['pmt'], request['pv'], request.get('fv', 0), request.get('type', 0), request.get('guess', 0.10)]
-    logger.info("Calling numpy.rate with args: {}".format(args))
-    return {'result': numpy.rate(*args)}
+    try:
+        validate(request, schema)
+        logger.info("Rate request: {}".format(request))
+        args = [request['nper'], request['pmt'], request['pv'], request.get('fv', 0), request.get('type', 0), request.get('guess', 0.10)]
+        logger.info("Calling numpy.rate with args: {}".format(args))
+        return {'result': numpy.rate(*args)}
+    except Exception as ex:
+        logger.error("Encountered an error while handling RATE request with args: {}. Exception: {}".format(request, ex))
+        raise ex
 
---
+++ code/validation_json_schemas.py
@@ -0,0 +1,188 @@
+fv_schema = {
+    "type": "object",
+    "properties": {
+        "rate": {
+            "type": "number"
+        },
+        "nper": {
+            "type": "number"
+        },
+        "pmt": {
+            "type": "number"
+        },
+        "pv": {
+            "type": "number"
+        },
+        "type": {
+            "type": "number"
+        }
+    },
+    "required": ["rate", "nper", "pmt", "pv"]
+}
+
+pv_schema = {
+    "type": "object",
+    "properties": {
+        "rate": {
+            "type": "number"
+        },
+        "nper": {
+            "type": "number"
+        },
+        "pmt": {
+            "type": "number"
+        },
+        "fv": {
+            "type": "number"
+        },
+        "type": {
+            "type": "number"
+        }
+    },
+    "required": ["rate", "nper", "pmt", "fv"]
+}
+
+npv_schema = {
+    "type": "object",
+    "properties": {
+        "rate": {
+            "type": "number"
+        },
+        "values": {
+            "type": "array",
+            "items": {
+                "type": "number",
+                "minItems": 1
+            }
+        }
+    },
+    "required": ["rate", "values"]
+}
+
+pmt_schema = {
+    "type": "object",
+    "properties": {
+        "rate": {
+            "type": "number"
+        },
+        "nper": {
+            "type": "number"
+        },
+        "pv": {
+            "type": "number"
+        },
+        "fv": {
+            "type": "number"
+        },
+        "type": {
+            "type": "number"
+        }
+    },
+    "required": ["rate", "nper", "pv"]
+}
+
+ppmt_schema = {
+    "type": "object",
+    "properties": {
+        "rate": {
+            "type": "number"
+        },
+        "per": {
+            "type": "number"
+        },
+        "nper": {
+            "type": "number"
+        },
+        "pv": {
+            "type": "number"
+        },
+        "fv": {
+            "type": "number"
+        },
+        "type": {
+            "type": "number"
+        }
+    },
+    "required": ["rate", "per", "nper", "pv"]
+}
+
+irr_schema = {
+    "type": "object",
+    "properties": {
+        "values": {
+            "type": "array",
+            "items": {
+                "type": "number",
+                "minItems": 1
+            }
+        }
+    },
+    "required": ["values"]
+}
+
+mirr_schema = {
+    "type": "object",
+    "properties": {
+        "values": {
+            "type": "array",
+            "items": {
+                "type": "number",
+                "minItems": 1
+            }
+        },
+        "finance_rate": {
+            "type": "number"
+        },
+        "reinvest_rate": {
+            "type": "number"
+        }
+    },
+    "required": ["values", "finance_rate", "reinvest_rate"]
+}
+
+nper_schema = {
+    "type": "object",
+    "properties": {
+        "rate": {
+            "type": "number"
+        },
+        "pmt": {
+            "type": "number"
+        },
+        "pv": {
+            "type": "number"
+        },
+        "fv": {
+            "type": "number"
+        },
+        "type": {
+            "type": "number"
+        }
+    },
+    "required": ["rate", "pmt", "pv"]
+}
+
+rate_schema = {
+    "type": "object",
+    "properties": {
+        "nper": {
+            "type": "number"
+        },
+        "pmt": {
+            "type": "number"
+        },
+        "pv": {
+            "type": "number"
+        },
+        "fv": {
+            "type": "number"
+        },
+        "type": {
+            "type": "number"
+        },
+        "guess": {
+            "type": "number"
+        }
+    },
+    "required": ["nper", "pmt", "pv", "fv"]
+}
